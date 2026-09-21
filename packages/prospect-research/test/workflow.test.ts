63f6beec6422b11c9f64d1d30c4e6b0a36f9256e
Dima Potmalnykov <potmalnicov@gmail.com>
2026-09-21 05:07:37 -0700

    Gate 13: include expected Steel identity in acceptance fixtures

--- packages/prospect-research/test/workflow.test.ts
+++ packages/prospect-research/test/workflow.test.ts
@@ -235,7 +235,12 @@ function acceptanceSample() {
         browserRuntime:
           "STEEL",
         browserBaseUrl:
-          "http://127.0.0.1:3000/"
+          "http://127.0.0.1:3000/",
+        browserExpectedImagePin:
+          "ghcr.io/steel-dev/steel-browser@sha256:" +
+          "a".repeat(64),
+        browserIdentityEvidence:
+          "EXPECTED_IMAGE_PIN_ONLY"
       },
       deliveryCostPlan: {
         version:
